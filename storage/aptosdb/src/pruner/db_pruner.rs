shaunstanislauslau/aptos-core

[Storage] [Pruner] Add more pruning functionality for ledger store pruner
Closes: #255

storage/aptosdb/src/pruner/db_pruner.rs
@@ -1,16 +1,43 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
+use aptos_logger::{error, info};
 use aptos_types::transaction::Version;
+use std::{cmp::min, thread::sleep, time::Duration};
 
 /// Defines the trait for pruner for different DB
 pub trait DBPruner {
-    /// Initialize the pruner and record the least readable version
-    fn initialize(&self);
+    /// Find out the first undeleted item in the stale node index.
+    ///
+    /// Seeking from the beginning (version 0) is potentially costly, we do it once upon worker
+    /// thread start, record the progress and seek from that position afterwards.
+    fn initialize(&self) {
+        loop {
+            match self.initialize_least_readable_version() {
+                Ok(least_readable_version) => {
+                    info!(
+                        least_readable_version = least_readable_version,
+                        "{} initialized.",
+                        self.get_pruner_name()
+                    );
+                    self.record_progress(least_readable_version);
+                    return;
+                }
+                Err(e) => {
+                    error!(
+                        error = ?e,
+                        "{} Error on first seek. Retrying in 1 second.", self.get_pruner_name()
+                    );
+                    sleep(Duration::from_secs(1));
+                }
+            }
+        }
+    }
+    fn get_pruner_name(&self) -> &'static str;
 
     /// Performs the actual pruning, a target version is passed, which is the target the pruner
     /// tries to prune.
-    fn prune(&self, max_versions: usize) -> anyhow::Result<Version>;
+    fn prune(&self, max_versions: Version) -> anyhow::Result<Version>;
 
     /// Initializes the least readable version stored in underlying DB storage
     fn initialize_least_readable_version(&self) -> anyhow::Result<Version>;
@@ -24,9 +51,21 @@ pub trait DBPruner {
     /// Returns the target version for the DB pruner
     fn target_version(&self) -> Version;
 
+    /// Returns the target version for the current pruning round - this might be different from the
+    /// target_version() because we need to keep max_version in account.
+    fn get_current_target_version(&self, max_versions: Version) -> Version {
+        // Current target version  might be less than the target version to ensure we don't prune
+        // more than max_version in one go.
+        min(
+            self.least_readable_version() + max_versions as u64,
+            self.target_version(),
+        )
+    }
     /// Records the current progress of the pruner by updating the least readable version
     fn record_progress(&self, least_readable_version: Version);
 
     /// True if there is pruning work pending to be done
-    fn is_pruning_pending(&self) -> bool;
+    fn is_pruning_pending(&self) -> bool {
+        self.target_version() >= self.least_readable_version()
+    }
 }

storage/aptosdb/src/pruner/ledger_store/ledger_store_pruner.rs
@@ -1,0 +1,78 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+use crate::{
+    metrics::DIEM_PRUNER_LEAST_READABLE_VERSION, pruner::db_pruner::DBPruner,
+    schema::ledger_counters::LedgerCountersSchema, LedgerStore,
+};
+use aptos_types::transaction::{AtomicVersion, Version};
+use schemadb::{ReadOptions, SchemaBatch, DB};
+use std::sync::{atomic::Ordering, Arc};
+
+pub const LEDGER_STORE_PRUNER_NAME: &str = "ledger store pruner";
+
+pub struct LedgerStorePruner {
+    db: Arc<DB>,
+    /// Keeps track of the target version that the pruner needs to achieve.
+    ledger_store: Arc<LedgerStore>,
+    target_version: AtomicVersion,
+    least_readable_version: AtomicVersion,
+}
+
+impl DBPruner for LedgerStorePruner {
+    fn get_pruner_name(&self) -> &'static str {
+        LEDGER_STORE_PRUNER_NAME
+    }
+
+    fn prune(&self, max_versions: Version) -> anyhow::Result<Version> {
+        let current_target_version = self.get_current_target_version(max_versions);
+        let mut db_batch = SchemaBatch::new();
+        self.ledger_store.prune_ledger_couners(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.db.write_schemas(db_batch)?;
+        self.record_progress(current_target_version);
+        Ok(current_target_version)
+    }
+
+    fn initialize_least_readable_version(&self) -> anyhow::Result<Version> {
+        let mut iter = self
+            .db
+            .iter::<LedgerCountersSchema>(ReadOptions::default())?;
+        iter.seek_to_first();
+        let version = iter.next().transpose()?.map_or(0, |(version, _)| version);
+        Ok(version)
+    }
+
+    fn least_readable_version(&self) -> Version {
+        self.least_readable_version.load(Ordering::Relaxed)
+    }
+
+    fn set_target_version(&self, target_version: Version) {
+        self.target_version.store(target_version, Ordering::Relaxed)
+    }
+
+    fn target_version(&self) -> Version {
+        self.target_version.load(Ordering::Relaxed)
+    }
+
+    fn record_progress(&self, least_readable_version: Version) {
+        self.least_readable_version
+            .store(least_readable_version, Ordering::Relaxed);
+        DIEM_PRUNER_LEAST_READABLE_VERSION
+            .with_label_values(&["ledger_store"])
+            .set(least_readable_version as i64);
+    }
+}
+
+impl LedgerStorePruner {
+    pub fn new(db: Arc<DB>, ledger_store: Arc<LedgerStore>) -> Self {
+        LedgerStorePruner {
+            db,
+            ledger_store,
+            target_version: AtomicVersion::new(0),
+            least_readable_version: AtomicVersion::new(0),
+        }
+    }
+}

storage/aptosdb/src/pruner/transaction_store/transaction_store_pruner.rs
@@ -1,0 +1,105 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+use crate::{
+    metrics::DIEM_PRUNER_LEAST_READABLE_VERSION, pruner::db_pruner::DBPruner,
+    transaction::TransactionSchema, TransactionStore,
+};
+use aptos_types::transaction::{AtomicVersion, Transaction, Version};
+use schemadb::{ReadOptions, SchemaBatch, DB};
+use std::sync::{atomic::Ordering, Arc};
+
+pub const TRANSACTION_STORE_PRUNER_NAME: &str = "transaction store pruner";
+
+pub struct TransactionStorePruner {
+    db: Arc<DB>,
+    transaction_store: Arc<TransactionStore>,
+    /// Keeps track of the target version that the pruner needs to achieve.
+    target_version: AtomicVersion,
+    least_readable_version: AtomicVersion,
+}
+
+impl DBPruner for TransactionStorePruner {
+    fn get_pruner_name(&self) -> &'static str {
+        TRANSACTION_STORE_PRUNER_NAME
+    }
+
+    fn prune(&self, max_versions: Version) -> anyhow::Result<Version> {
+        let least_readable_version = self.least_readable_version();
+        // Current target version  might be less than the target version to ensure we don't prune
+        // more than max_version in one go.
+        let current_target_version = self.get_current_target_version(max_versions);
+        let candidate_transactions = self
+            .get_pruning_candidate_transactions(least_readable_version, current_target_version)?;
+        let mut db_batch = SchemaBatch::new();
+        self.transaction_store
+            .prune_transaction_by_hash(&candidate_transactions, &mut db_batch)?;
+        self.transaction_store
+            .prune_transaction_by_account(&candidate_transactions, &mut db_batch)?;
+        self.transaction_store.prune_transaction_schema(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.transaction_store.prune_transaction_info_schema(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.transaction_store.prune_transaction_accumulator(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.db.write_schemas(db_batch)?;
+
+        self.record_progress(current_target_version);
+        Ok(current_target_version)
+    }
+
+    fn initialize_least_readable_version(&self) -> anyhow::Result<Version> {
+        let mut iter = self.db.iter::<TransactionSchema>(ReadOptions::default())?;
+        iter.seek_to_first();
+        let version = iter.next().transpose()?.map_or(0, |(version, _)| version);
+        Ok(version)
+    }
+
+    fn least_readable_version(&self) -> Version {
+        self.least_readable_version.load(Ordering::Relaxed)
+    }
+
+    fn set_target_version(&self, target_version: Version) {
+        self.target_version.store(target_version, Ordering::Relaxed)
+    }
+
+    fn target_version(&self) -> Version {
+        self.target_version.load(Ordering::Relaxed)
+    }
+    fn record_progress(&self, least_readable_version: Version) {
+        self.least_readable_version
+            .store(least_readable_version, Ordering::Relaxed);
+        DIEM_PRUNER_LEAST_READABLE_VERSION
+            .with_label_values(&["transaction_store"])
+            .set(least_readable_version as i64);
+    }
+}
+
+impl TransactionStorePruner {
+    pub(in crate::pruner) fn new(db: Arc<DB>, transaction_store: Arc<TransactionStore>) -> Self {
+        TransactionStorePruner {
+            db,
+            transaction_store,
+            target_version: AtomicVersion::new(0),
+            least_readable_version: AtomicVersion::new(0),
+        }
+    }
+
+    fn get_pruning_candidate_transactions(
+        &self,
+        start: Version,
+        end: Version,
+    ) -> anyhow::Result<Vec<Transaction>> {
+        self.transaction_store
+            .get_transaction_iter(start, (end - start) as usize)?
+            .collect()
+    }
+}

storage/aptosdb/src/pruner/transaction_store/write_set_pruner.rs
@@ -1,0 +1,78 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+use crate::{
+    metrics::DIEM_PRUNER_LEAST_READABLE_VERSION, pruner::db_pruner::DBPruner,
+    write_set::WriteSetSchema, TransactionStore,
+};
+use aptos_types::transaction::{AtomicVersion, Version};
+use schemadb::{ReadOptions, SchemaBatch, DB};
+use std::sync::{atomic::Ordering, Arc};
+
+pub const TRANSACTION_STORE_PRUNER_NAME: &str = "transaction store pruner";
+
+pub struct WriteSetPruner {
+    db: Arc<DB>,
+    transaction_store: Arc<TransactionStore>,
+    /// Keeps track of the target version that the pruner needs to achieve.
+    target_version: AtomicVersion,
+    least_readable_version: AtomicVersion,
+}
+
+impl DBPruner for WriteSetPruner {
+    fn get_pruner_name(&self) -> &'static str {
+        TRANSACTION_STORE_PRUNER_NAME
+    }
+
+    fn prune(&self, max_versions: Version) -> anyhow::Result<Version> {
+        // Current target version  might be less than the target version to ensure we don't prune
+        // more than max_version in one go.
+        let mut db_batch = SchemaBatch::new();
+        let current_target_version = self.get_current_target_version(max_versions);
+        self.transaction_store.prune_write_set(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.db.write_schemas(db_batch)?;
+
+        self.record_progress(current_target_version);
+        Ok(current_target_version)
+    }
+
+    fn initialize_least_readable_version(&self) -> anyhow::Result<Version> {
+        let mut iter = self.db.iter::<WriteSetSchema>(ReadOptions::default())?;
+        iter.seek_to_first();
+        let version = iter.next().transpose()?.map_or(0, |(version, _)| version);
+        Ok(version)
+    }
+
+    fn least_readable_version(&self) -> Version {
+        self.least_readable_version.load(Ordering::Relaxed)
+    }
+
+    fn set_target_version(&self, target_version: Version) {
+        self.target_version.store(target_version, Ordering::Relaxed)
+    }
+
+    fn target_version(&self) -> Version {
+        self.target_version.load(Ordering::Relaxed)
+    }
+    fn record_progress(&self, least_readable_version: Version) {
+        self.least_readable_version
+            .store(least_readable_version, Ordering::Relaxed);
+        DIEM_PRUNER_LEAST_READABLE_VERSION
+            .with_label_values(&["write_set"])
+            .set(least_readable_version as i64);
+    }
+}
+
+impl WriteSetPruner {
+    pub(in crate::pruner) fn new(db: Arc<DB>, transaction_store: Arc<TransactionStore>) -> Self {
+        WriteSetPruner {
+            db,
+            transaction_store,
+            target_version: AtomicVersion::new(0),
+            least_readable_version: AtomicVersion::new(0),
+        }
+    }
+}

sitalkedia/aptos-core-1

storage/aptosdb/src/pruner/db_pruner.rs
@@ -1,16 +1,43 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
+use aptos_logger::{error, info};
 use aptos_types::transaction::Version;
+use std::{cmp::min, thread::sleep, time::Duration};
 
 /// Defines the trait for pruner for different DB
 pub trait DBPruner {
-    /// Initialize the pruner and record the least readable version
-    fn initialize(&self);
+    /// Find out the first undeleted item in the stale node index.
+    ///
+    /// Seeking from the beginning (version 0) is potentially costly, we do it once upon worker
+    /// thread start, record the progress and seek from that position afterwards.
+    fn initialize(&self) {
+        loop {
+            match self.initialize_least_readable_version() {
+                Ok(least_readable_version) => {
+                    info!(
+                        least_readable_version = least_readable_version,
+                        "{} initialized.",
+                        self.name()
+                    );
+                    self.record_progress(least_readable_version);
+                    return;
+                }
+                Err(e) => {
+                    error!(
+                        error = ?e,
+                        "{} Error on first seek. Retrying in 1 second.", self.name()
+                    );
+                    sleep(Duration::from_secs(1));
+                }
+            }
+        }
+    }
+    fn name(&self) -> &'static str;
 
     /// Performs the actual pruning, a target version is passed, which is the target the pruner
     /// tries to prune.
-    fn prune(&self, max_versions: usize) -> anyhow::Result<Version>;
+    fn prune(&self, max_versions: Version) -> anyhow::Result<Version>;
 
     /// Initializes the least readable version stored in underlying DB storage
     fn initialize_least_readable_version(&self) -> anyhow::Result<Version>;
@@ -24,9 +51,21 @@ pub trait DBPruner {
     /// Returns the target version for the DB pruner
     fn target_version(&self) -> Version;
 
+    /// Returns the target version for the current pruning round - this might be different from the
+    /// target_version() because we need to keep max_version in account.
+    fn get_currrent_batch_target(&self, max_versions: Version) -> Version {
+        // Current target version  might be less than the target version to ensure we don't prune
+        // more than max_version in one go.
+        min(
+            self.least_readable_version() + max_versions as u64,
+            self.target_version(),
+        )
+    }
     /// Records the current progress of the pruner by updating the least readable version
     fn record_progress(&self, least_readable_version: Version);
 
     /// True if there is pruning work pending to be done
-    fn is_pruning_pending(&self) -> bool;
+    fn is_pruning_pending(&self) -> bool {
+        self.target_version() >= self.least_readable_version()
+    }
 }

storage/aptosdb/src/pruner/ledger_store/ledger_store_pruner.rs
@@ -1,0 +1,78 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+use crate::{
+    metrics::DIEM_PRUNER_LEAST_READABLE_VERSION, pruner::db_pruner::DBPruner,
+    schema::ledger_counters::LedgerCountersSchema, LedgerStore,
+};
+use aptos_types::transaction::{AtomicVersion, Version};
+use schemadb::{ReadOptions, SchemaBatch, DB};
+use std::sync::{atomic::Ordering, Arc};
+
+pub const LEDGER_STORE_PRUNER_NAME: &str = "ledger store pruner";
+
+pub struct LedgerStorePruner {
+    db: Arc<DB>,
+    /// Keeps track of the target version that the pruner needs to achieve.
+    ledger_store: Arc<LedgerStore>,
+    target_version: AtomicVersion,
+    least_readable_version: AtomicVersion,
+}
+
+impl DBPruner for LedgerStorePruner {
+    fn name(&self) -> &'static str {
+        LEDGER_STORE_PRUNER_NAME
+    }
+
+    fn prune(&self, max_versions: Version) -> anyhow::Result<Version> {
+        let current_target_version = self.get_currrent_batch_target(max_versions);
+        let mut db_batch = SchemaBatch::new();
+        self.ledger_store.prune_ledger_couners(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.db.write_schemas(db_batch)?;
+        self.record_progress(current_target_version);
+        Ok(current_target_version)
+    }
+
+    fn initialize_least_readable_version(&self) -> anyhow::Result<Version> {
+        let mut iter = self
+            .db
+            .iter::<LedgerCountersSchema>(ReadOptions::default())?;
+        iter.seek_to_first();
+        let version = iter.next().transpose()?.map_or(0, |(version, _)| version);
+        Ok(version)
+    }
+
+    fn least_readable_version(&self) -> Version {
+        self.least_readable_version.load(Ordering::Relaxed)
+    }
+
+    fn set_target_version(&self, target_version: Version) {
+        self.target_version.store(target_version, Ordering::Relaxed)
+    }
+
+    fn target_version(&self) -> Version {
+        self.target_version.load(Ordering::Relaxed)
+    }
+
+    fn record_progress(&self, least_readable_version: Version) {
+        self.least_readable_version
+            .store(least_readable_version, Ordering::Relaxed);
+        DIEM_PRUNER_LEAST_READABLE_VERSION
+            .with_label_values(&["ledger_store"])
+            .set(least_readable_version as i64);
+    }
+}
+
+impl LedgerStorePruner {
+    pub fn new(db: Arc<DB>, ledger_store: Arc<LedgerStore>) -> Self {
+        LedgerStorePruner {
+            db,
+            ledger_store,
+            target_version: AtomicVersion::new(0),
+            least_readable_version: AtomicVersion::new(0),
+        }
+    }
+}

storage/aptosdb/src/pruner/transaction_store/transaction_store_pruner.rs
@@ -1,0 +1,105 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+use crate::{
+    metrics::DIEM_PRUNER_LEAST_READABLE_VERSION, pruner::db_pruner::DBPruner,
+    transaction::TransactionSchema, TransactionStore,
+};
+use aptos_types::transaction::{AtomicVersion, Transaction, Version};
+use schemadb::{ReadOptions, SchemaBatch, DB};
+use std::sync::{atomic::Ordering, Arc};
+
+pub const TRANSACTION_STORE_PRUNER_NAME: &str = "transaction store pruner";
+
+pub struct TransactionStorePruner {
+    db: Arc<DB>,
+    transaction_store: Arc<TransactionStore>,
+    /// Keeps track of the target version that the pruner needs to achieve.
+    target_version: AtomicVersion,
+    least_readable_version: AtomicVersion,
+}
+
+impl DBPruner for TransactionStorePruner {
+    fn name(&self) -> &'static str {
+        TRANSACTION_STORE_PRUNER_NAME
+    }
+
+    fn prune(&self, max_versions: Version) -> anyhow::Result<Version> {
+        let least_readable_version = self.least_readable_version();
+        // Current target version  might be less than the target version to ensure we don't prune
+        // more than max_version in one go.
+        let current_target_version = self.get_currrent_batch_target(max_versions);
+        let candidate_transactions = self
+            .get_pruning_candidate_transactions(least_readable_version, current_target_version)?;
+        let mut db_batch = SchemaBatch::new();
+        self.transaction_store
+            .prune_transaction_by_hash(&candidate_transactions, &mut db_batch)?;
+        self.transaction_store
+            .prune_transaction_by_account(&candidate_transactions, &mut db_batch)?;
+        self.transaction_store.prune_transaction_schema(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.transaction_store.prune_transaction_info_schema(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.transaction_store.prune_transaction_accumulator(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.db.write_schemas(db_batch)?;
+
+        self.record_progress(current_target_version);
+        Ok(current_target_version)
+    }
+
+    fn initialize_least_readable_version(&self) -> anyhow::Result<Version> {
+        let mut iter = self.db.iter::<TransactionSchema>(ReadOptions::default())?;
+        iter.seek_to_first();
+        let version = iter.next().transpose()?.map_or(0, |(version, _)| version);
+        Ok(version)
+    }
+
+    fn least_readable_version(&self) -> Version {
+        self.least_readable_version.load(Ordering::Relaxed)
+    }
+
+    fn set_target_version(&self, target_version: Version) {
+        self.target_version.store(target_version, Ordering::Relaxed)
+    }
+
+    fn target_version(&self) -> Version {
+        self.target_version.load(Ordering::Relaxed)
+    }
+    fn record_progress(&self, least_readable_version: Version) {
+        self.least_readable_version
+            .store(least_readable_version, Ordering::Relaxed);
+        DIEM_PRUNER_LEAST_READABLE_VERSION
+            .with_label_values(&["transaction_store"])
+            .set(least_readable_version as i64);
+    }
+}
+
+impl TransactionStorePruner {
+    pub(in crate::pruner) fn new(db: Arc<DB>, transaction_store: Arc<TransactionStore>) -> Self {
+        TransactionStorePruner {
+            db,
+            transaction_store,
+            target_version: AtomicVersion::new(0),
+            least_readable_version: AtomicVersion::new(0),
+        }
+    }
+
+    fn get_pruning_candidate_transactions(
+        &self,
+        start: Version,
+        end: Version,
+    ) -> anyhow::Result<Vec<Transaction>> {
+        self.transaction_store
+            .get_transaction_iter(start, (end - start) as usize)?
+            .collect()
+    }
+}

storage/aptosdb/src/pruner/transaction_store/write_set_pruner.rs
@@ -1,0 +1,78 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+use crate::{
+    metrics::DIEM_PRUNER_LEAST_READABLE_VERSION, pruner::db_pruner::DBPruner,
+    write_set::WriteSetSchema, TransactionStore,
+};
+use aptos_types::transaction::{AtomicVersion, Version};
+use schemadb::{ReadOptions, SchemaBatch, DB};
+use std::sync::{atomic::Ordering, Arc};
+
+pub const TRANSACTION_STORE_PRUNER_NAME: &str = "transaction store pruner";
+
+pub struct WriteSetPruner {
+    db: Arc<DB>,
+    transaction_store: Arc<TransactionStore>,
+    /// Keeps track of the target version that the pruner needs to achieve.
+    target_version: AtomicVersion,
+    least_readable_version: AtomicVersion,
+}
+
+impl DBPruner for WriteSetPruner {
+    fn name(&self) -> &'static str {
+        TRANSACTION_STORE_PRUNER_NAME
+    }
+
+    fn prune(&self, max_versions: Version) -> anyhow::Result<Version> {
+        // Current target version  might be less than the target version to ensure we don't prune
+        // more than max_version in one go.
+        let mut db_batch = SchemaBatch::new();
+        let current_target_version = self.get_currrent_batch_target(max_versions);
+        self.transaction_store.prune_write_set(
+            self.least_readable_version(),
+            current_target_version,
+            &mut db_batch,
+        )?;
+        self.db.write_schemas(db_batch)?;
+
+        self.record_progress(current_target_version);
+        Ok(current_target_version)
+    }
+
+    fn initialize_least_readable_version(&self) -> anyhow::Result<Version> {
+        let mut iter = self.db.iter::<WriteSetSchema>(ReadOptions::default())?;
+        iter.seek_to_first();
+        let version = iter.next().transpose()?.map_or(0, |(version, _)| version);
+        Ok(version)
+    }
+
+    fn least_readable_version(&self) -> Version {
+        self.least_readable_version.load(Ordering::Relaxed)
+    }
+
+    fn set_target_version(&self, target_version: Version) {
+        self.target_version.store(target_version, Ordering::Relaxed)
+    }
+
+    fn target_version(&self) -> Version {
+        self.target_version.load(Ordering::Relaxed)
+    }
+    fn record_progress(&self, least_readable_version: Version) {
+        self.least_readable_version
+            .store(least_readable_version, Ordering::Relaxed);
+        DIEM_PRUNER_LEAST_READABLE_VERSION
+            .with_label_values(&["write_set"])
+            .set(least_readable_version as i64);
+    }
+}
+
+impl WriteSetPruner {
+    pub(in crate::pruner) fn new(db: Arc<DB>, transaction_store: Arc<TransactionStore>) -> Self {
+        WriteSetPruner {
+            db,
+            transaction_store,
+            target_version: AtomicVersion::new(0),
+            least_readable_version: AtomicVersion::new(0),
+        }
+    }
+}